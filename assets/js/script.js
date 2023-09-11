a26f9798e89b53c45c7cd976b26d8d92fd8dd43f
symox <symoxn7@gmail.com>
2023-09-11 18:20:00 +0100

    HTML timeblocks, and JQuery, DayJS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,41 +4,50 @@ $(document).ready(function () {
   let currentDate = dayjs();
   $("#currentDay").text(currentDate);
 
-  //Timeblocks added to HTML, for each hour of the day
-
-  let dayHours = [
-    "9am",
-    "10am",
-    "11am",
-    "12am",
-    "1pm",
-    "2pm",
-    "3pm",
-    "4pm",
-    "5pm",
-  ];
-
-  for (i = 0; i < dayHours.length; i++) {
-    let timeBlock = $("<div>");
-    timeBlock.addClass("time-block row");
-    timeBlock.text(dayHours[i]);
-    $(".container").append(timeBlock);
-  };
-
-
-  //Add text input to time block
-  let calendarRows = $(".time-block") 
-  let calendarItems = $("<input>");
-  calendarItems.addClass("description");
-  calendarRows.append(calendarItems)
-
-  //Add a save button to each one to save text to local storage
-  let saveButton = $("<button>");
-  saveButton.addClass("saveBtn", "saveBtn i:hover")
-  calendarRows.append(saveButton)
-
   //Add event listener to save button
+  $(".saveBtn").on("click", function(){
+    
+    let value = $(this).siblings(".description").val();
+    let timeId = $(this).parent().attr("id");
 
-  
+
+    localStorage.setItem(timeId, value)
+    $()
+
+  })
+
   //Color changes based on current time
-});
+  function hourUpdate(){
+    let currentHour = moment().hours();
+    
+    for(let i = 0; i < $(".time-block").length; i++){
+      let hour = parseInt($(".time-block")[i].getAttribute("id").split("-")[1])
+      console.log(currentHour)
+      console.log(hour)
+      if (hour < currentHour) {
+        $(".time-block")[i].classList.add("past")
+      } else if (hour === currentHour) {
+        $(".time-block")[i].classList.add("past")
+        $(".time-block")[i].classList.remove("present")
+      } else {
+        $(".time-block")[i].classList.remove("past")
+        $(".time-block")[i].classList.remove("present")
+        $(".time-block")[i].classList.add("future")
+      }
+    }
+  }
+
+  let interval = setInterval(hourUpdate, 15000);
+
+  //Remember calendar entries
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+  
+})
